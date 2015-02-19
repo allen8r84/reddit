ff13d6db4c006e4de319fd0501bf33c3a6f2205e
Jon <jon@allen8r.com>
2015-02-18 19:27:07 -0700

    finish step 2 working on step 3

--- service.js
+++ service.js
@@ -1,6 +1,19 @@
 var app = angular.module('reddit');
 
-app.service('FirebaseService', function() {
-    
+app.service('FirebaseService', function($http, $q) {
+    //https://devmtn.firebaseio.com/posts.json
+    //DevMtn
+    debugger // use this anywhere to debug code
+    this.getdata = function() {
+        var deferred = $q.defer();
+        $http({
+            method: 'GET',
+            url: 'https://devmtn.firebaseio.com/posts.json'
+        }).then(function(response){
+            response = response.data;
+            deferred.resolve(response);
+        });
+        return deferred.promise;
+    };
     
 });
