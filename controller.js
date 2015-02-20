882a3a9eade9e2e356eccadda60cce3ab13efc9f
Jon <jon@allen8r.com>
2015-02-19 19:19:18 -0700

    finish project, but with many errors that I am struggling with debugging - I am going to start over with different files...

--- controller.js
+++ controller.js
@@ -7,5 +7,38 @@ app.controller('PostsController', function($scope, FirebaseService) {
             $scope.posts = res;
         });
     };
+    
     $scope.getPosts();
+    
+    $scope.addPost = function() {
+        FirebaseService.addPost($scope.newPost).then(function(res) {;
+            getData();
+        });
+        $scope.reset();
+    };
+    $scope.reset = function() {
+        $scope.newPost = {};
+    };
+    setInterval(function() {
+        $scope.getPosts();     
+    }, 1500);
+    
+    $scope.vote = function(id, direction) {
+        FirebaseService.vote(id, direction, $scope.posts[id].karma).then(function(data) {
+            $scope.getPosts();
+        });
+    };
+    
+    $scope.submitComment = function(id, comment) {
+        var commentObj = {};
+        commentObj.text = comment;
+        commentObj.timestamp = Date.now();
+        
+        FirebaseService.addComment(id, commentObj).then(function(data) {
+          getData();  
+        })
+        
+        
+    }
+    
 });
--- service.js
+++ service.js
@@ -13,4 +13,46 @@ app.service('FirebaseService', function($http, $q) {
         return deferred.promise;
     };
     
+    this.addPost = function(newPost) {
+        newPost.timestamp = Date.now();
+        newPost.comments = [];
+        newPost.karma = 0;
+        newPost.id = guid();
+        
+        var deferred = $q.defer();
+        
+        $http.put('https://devmtn.firebaseio.com/posts/' + newPost.id + '.json', newPost).then(function(res){
+            defered.resolve(res);
+        })
+       
+    }
+    
+    this.vote = function(postId, direction, karma) {
+        if(direction === 'up') {
+            karma++;
+        }else if (direction === 'down') {
+            karma--;
+        }else {
+            console.log('karma isn\'t working');
+        }
+        return $http.patch('https://devmtn.firebaseio.com/posts/' + postId + '.json', {'karma': karma}).then(function(data){
+           
+        })
+    }
+    
+    this.addComment = function(postId, commentObj) {
+        return $http.post('https://devmtn.firebaseio.com/posts/' + postId + '/copmments.json', {comments: commentObj});
+    }
+
+    
+    var guid = function() {
+    var s4 = function() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
+    
 });
